9842f883968554d88d6c5dc43606eda15ca51d95
zeeshan ahamd <zeeshan@craxxino.com>
2022-02-14 21:00:42 +0530

    Added Global Error Handler

---
+++ utils/ErrorClass.js
@@ -0,0 +1,11 @@
+// create a error class
+class HandleError extends Error{
+    constructor(message, statusCode) {
+        super(message)
+        this.statusCode = statusCode
+        this.status = `${statusCode}`.startsWith("4") ? "fail" : "error"
+        this.isOperational = true
+        Error.captureStackTrace(this, this.constructor)
+    }
+}
+module.exports = HandleError
